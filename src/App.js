12127af49d56b821d1d1a8c460e5df136ceb9e4e
TsunenoriHenna <tsunenori.henna@dragon.jp>
2021-07-18 16:45:03 +0900

    larn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-
+import React, { Component } from 'react';
+/*
 function App() {
-  return (<div><h1>Hello World!</h1></div>);
+  return (<div>Hello World!</div>);
 }
-
+*/
+class App extends Component{
+/*
+  render() {
+   const change = "Hello Japan";
+   const dom = <h1 className="test">{change}</h1>;
+   return dom;
+*/
+/*
+   return React.createElement(
+	"h1",
+	null,
+	"Hello, World"
+   );
+*/
+  render() {
+    return <input type="text" onClick={() => {console.log("text click")}}     />;
+  }
+}
 export default App;
